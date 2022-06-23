9a3c485a470c8203849ce62b6e734f7f38bd046c
nickpeters741 <npeters741@gmail.com>
2022-06-23 15:06:18 +0300

    linking sanity cms to react

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,14 +3,19 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants'
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: 'web development', description: 'i am a good web developer.', imgUrl: images.about01},
-  { title: 'web design', description: 'i am a good web developer.', imgUrl: images.about02},
-  { title: 'UI/UX', description: 'i am a good web developer.', imgUrl: images.about03},
-  { title: 'web animations', description: 'i am a good web developer.', imgUrl: images.about04},
-];
+
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">i enjoy playing<span> around with </span><br /> technology to <span>build awesome solutions</span>
@@ -25,7 +30,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-              <img src={about.imgUrl} alt={about.title}/>
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className="bold-text" style={{marginTop: 20 }}>{about.title}</h2>
               <p className="p-text" style={{marginTop: 10 }}>{about.description}</p>
           </motion.div>
